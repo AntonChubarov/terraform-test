12bc3fb0b0a5eb78d39ca450226d73d12bb08c79
AntonChubarov <achlp600@gmail.com>
2024-03-08 17:33:07 -0500

    feat: creating lambda using zip archive

--- formations/lambda/main.tf
+++ formations/lambda/main.tf
@@ -1,6 +1,103 @@
-resource "null_resource" "health_check" {
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    archive = {
+      source = "hashicorp/archive"
+    }
+    null = {
+      source = "hashicorp/null"
+    }
+  }
 
-  provisioner "local-exec" {
-    command = "/bin/bash build.sh test_lambda"
+  required_version = ">= 1.7.4"
+}
+
+provider "aws" {
+  region  = var.region
+  profile = "default"
+
+  default_tags {
+    tags = {
+      app = "lambda-deploy-test"
+      environment = var.environment
+    }
   }
 }
+
+data "aws_iam_policy_document" "assume_lambda_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "lambda" {
+  name               = "${var.environment}-test-fn-role"
+  description        = "Role for lambda to assume lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_lambda_role.json
+}
+
+data "aws_iam_policy_document" "allow_lambda_logging" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = [
+      "arn:aws:logs:*:*:*",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "function_logging_policy" {
+  name        = "AllowLambdaLoggingPolicy"
+  description = "Policy for lambda cloudwatch logging"
+  policy      = data.aws_iam_policy_document.allow_lambda_logging.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logging_policy_attachment" {
+  role       = aws_iam_role.lambda.id
+  policy_arn = aws_iam_policy.function_logging_policy.arn
+}
+
+resource "null_resource" "function_binary" {
+  provisioner "local-exec" {
+    command = "GOOS=linux GOARCH=amd64 CGO_ENABLED=0 GOFLAGS=-trimpath go build -mod=readonly -ldflags='-s -w' -o bootstrap main.go"
+  }
+}
+
+data "archive_file" "function_archive" {
+  depends_on = [null_resource.function_binary]
+
+  type        = "zip"
+  source_file = "bootstrap"
+  output_path = "bootstrap.zip"
+}
+
+resource "aws_lambda_function" "function" {
+  function_name = "${var.environment}-test-fn"
+  description   = "My test function"
+  role          = aws_iam_role.lambda.arn
+  handler       = "bootstrap"
+  memory_size   = 128
+
+  filename         = "bootstrap.zip"
+  source_code_hash = data.archive_file.function_archive.output_base64sha256
+
+  runtime = "provided.al2"
+}
+
+#resource "null_resource" "health_check" {
+#
+#  provisioner "local-exec" {
+#    command = "/bin/bash build.sh test_lambda"
+#  }
+#}
